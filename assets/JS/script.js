0ac688e4b1c53badd9d0e436d3bad99daeb4583f
Felipe de Brito Santos <felipebrito.santos2005@gmail.com>
2023-07-05 15:22:54 -0300

    update

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,15 +1,29 @@
+var botaoSim = document.getElementById("sim");
+
+botaoSim.addEventListener("click", function() {
+  window.location.href = "../Two_years_of_Dating/assets/index2.html"; // Substitua "https://www.seulink.com" pelo link desejado
+});
+
+
 function fuja() {
     var botaoNao = document.getElementById("nao");
-    var painel = document.querySelector(".painel");
-    
-    var painelRect = painel.getBoundingClientRect();
-    
-    var maxX = painelRect.width - 2.5*botaoNao.offsetWidth;
-    var maxY = painelRect.height - 2.5*botaoNao.offsetHeight;
-    
+    var container = document.querySelector(".container");
+  
+    var containerRect = container.getBoundingClientRect();
+    var containerLeft = containerRect.left;
+    var containerTop = containerRect.top;
+  
+    var containerWidth = containerRect.width;
+    var containerHeight = containerRect.height;
+  
+    var maxX = containerWidth - botaoNao.offsetWidth;
+    var maxY = containerHeight - botaoNao.offsetHeight;
+  
     var aleatorioX = Math.floor(Math.random() * maxX);
     var aleatorioY = Math.floor(Math.random() * maxY);
-    
-    botaoNao.style.left = (painelRect.left + aleatorioX) + "px";
-    botaoNao.style.top = (painelRect.top + aleatorioY) + "px";
+  
+    botaoNao.style.position = "absolute";
+    botaoNao.style.left = (containerLeft + aleatorioX) + "px";
+    botaoNao.style.top = (containerTop + aleatorioY) + "px";
 }
+
